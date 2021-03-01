4ec432321605fa900baa1a89f3bd77679d350385
CockHeroJoe <RoughRavager@gmail.com>
2021-02-28 21:50:52 -0500

    Improved ImageMagic Binary resolution

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 #! /usr/bin/env python3
 import os
 
-from moviepy.config import change_settings
+from moviepy.config import change_settings as moviepy_change_settings
 
 from parsing import OutputConfig, parse_command_line_args
 from gui import GUI
 
 
-# Overrride moviepy resolution bug
+# Override moviepy's binary filepath resolution bug in Windows
 if os.name == "nt":
-    import winreg as wr 
-    key = wr.OpenKey(wr.HKEY_LOCAL_MACHINE, "SOFTWARE\\ImageMagick\\Current")
-    binary = wr.QueryValueEx(key, "BinPath")[0] + "\\magick.exe"
-    key.Close()
-    change_settings({"IMAGEMAGICK_BINARY": binary})
+    import winreg as wr
+    try:
+        with wr.OpenKey(
+                wr.HKEY_LOCAL_MACHINE,
+                "SOFTWARE\\ImageMagick\\Current") as key:
+            binary = wr.QueryValueEx(key, "BinPath")[0] + "\\magick.exe"
+            moviepy_change_settings({"IMAGEMAGICK_BINARY": binary})
+    except OSError:
+        print(OSError)
+        print("\nIs ImageMagic Installed?\n")
+        exit(1)
 
 
 def main():
