cd829f4b01a32997d619a9e201a31ea7154c520d
CockHeroJoe <RoughRavager@gmail.com>
2021-02-28 21:53:01 -0500

    replaced pygame with moviepy

--- preview.py
+++ preview.py
@@ -1,144 +1,182 @@
 import numpy as np
-import pygame as pg
+import PIL.Image
+import PIL.ImageTk
 import threading
 import time
+import tkinter
 import math
-from moviepy.decorators import convert_masks_to_RGB, requires_duration
-from moviepy.video.io.preview import imdisplay
+# from moviepy.decorators import convert_masks_to_RGB, requires_duration
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.VideoClip import VideoClip
 from moviepy.video.compositing.CompositeVideoClip import clips_array
+from moviepy.video.fx.resize import resize
 
 from constants import DISPLAY_SIZE
 
-NUMBER_KEYS = [  # pg.key_code(str(k)) for k in range(1, 10)]
-    pg.K_1,
-    pg.K_2,
-    pg.K_3,
-    pg.K_4,
-    pg.K_5,
-    pg.K_6,
-    pg.K_7,
-    pg.K_8,
-    pg.K_9,
-]
+
+class ResizingCanvas(tkinter.Canvas):
+    # from https://stackoverflow.com/questions/22835289/
+    def __init__(self, parent, **kwargs):
+        tkinter.Canvas.__init__(self, parent, **kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self, event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all", 0, 0, wscale, hscale)
 
 
-def preview_all(out_clips: [VideoClip], display_size=DISPLAY_SIZE, fps=15
-                ) -> int:
-    square_size = math.ceil(math.sqrt(len(out_clips)))
-    segments = [
-        [
-            out_clips[i + j].resize(1 / square_size)
-            for j in range(square_size)
-            if i + j < len(out_clips)
-        ] for i in range(0, len(out_clips), square_size)
-    ]
-    square = clips_array(segments).resize(display_size).without_audio()
-    pg.display.set_caption("Choose which version to use")
-    chosen = None
-    while None == chosen:
-        # Preview all versions
-        result = preview(square, fps=fps)
-        # Check which version was chosen
-        if result == None:
-            pg.quit()
-            raise InterruptedError("Switching to single version")
-        elif type(result) == int:
-            chosen = result - 1
+class PreviewGUI:
+    def __init__(self,
+                 clips: [VideoClip],
+                 choose: lambda: int,
+                 display_size: (int, int) = DISPLAY_SIZE,
+                 fps: int = 15):
+        self.window = tkinter.Tk()
+        self.window.title("Preview: Choose which version to use")
+        self.display_size = display_size
+        self._choose = choose
+        self.fps = fps
+        self.update_id = None
+        self.is_full_screen = False
+        self.num_versions = len(clips)
+
+        # Create a canvas that can fit the above video source size
+        self.canvas = ResizingCanvas(self.window,
+                                     width=display_size[0],
+                                     height=display_size[1],
+                                     highlightthickness=0)
+        self.canvas.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+
+        # Add handlers to canvas for input
+        for key in [str(k) for k in range(1, len(clips) + 1)]:
+            self.window.bind(key, self.on_number_key)
+        self.window.bind("<Button-1>", self.on_click)
+        self.window.bind("<Escape>", self.exit)
+        self.window.bind('f', self.toggle_full_screen)
+        self.window.bind('r', self.refresh_sources)
+        self.window.bind("<Left>", self.scrub_backward)
+        self.window.bind("<Right>", self.scrub_forward)
+        self.window.bind("<Key-space>", lambda e: self.pause()
+                         if self.is_playing else self.play())
+
+        # Prepare the clips by combining them into a composite clip
+        self.square_size = math.ceil(math.sqrt(len(clips)))
+        segments = [
+            [
+                resize(clips[i + j], 1 / self.square_size)
+                for j in range(self.square_size)
+                if i + j < len(clips)
+            ] for i in range(0, len(clips), self.square_size)
+        ]
+
+        self.native_video = resize(clips_array(segments), self.display_size)
+        # TODO: fix fullscreen
+        # self.fullscreen_video = resize(
+        #   clips_array(segments), height=self.window.winfo_screenheight())
+        self.video = self.native_video
+
+        self.dt = 1.0 / fps
+        self.t = 0
+        self.is_playing = False
+        self.display()
+
+    def run(self):
+        self.play()
+        self.window.mainloop()
+        self.window.destroy()
+
+    def play(self):
+        self.is_playing = True
+        self.t0 = time.time() - self.t
+        self.update()
+
+    def pause(self):
+        self.is_playing = False
+        if self.update_id is not None:
+            self.window.after_cancel(self.update_id)
+
+    def scrub_backward(self, _: tkinter.Event = None):
+        adjust = 5.0 if self.is_playing else 1.0 / self.fps
+        if adjust > self.t:
+            adjust = self.t
+        self.t0 += adjust
+        self.t -= adjust
+        self.display()
+
+    def scrub_forward(self, _: tkinter.Event = None):
+        adjust = 5.0 if self.is_playing else 1.0 / self.fps
+        remainder = self.video.duration - self.t
+        if adjust > remainder:
+            adjust = remainder
+
+        self.t0 -= adjust
+        self.t += adjust
+        self.display()
+
+    def exit(self, _: tkinter.Event = None):
+        self.pause()
+        self.window.quit()
+
+    def on_click(self, event: tkinter.Event):
+        x_dim, y_dim = self.display_size
+        x = math.floor(event.x / x_dim * self.square_size)
+        y = math.floor(event.y / y_dim * self.square_size)
+        choice = y * self.square_size + x
+        if choice < 0 or choice > self.num_versions:
+            return
         else:
-            x = math.floor(result[0] / display_size[0] * square_size)
-            y = math.floor(result[1] / display_size[1] * square_size)
-            chosen = y * square_size + x
-        if chosen < 0 or chosen >= len(out_clips):
-            print("Clip #{} not a valid clip in [1, {}]".format(
-                1 + chosen, len(out_clips)
-            ))
-            chosen = None
-    return chosen
+            self._choose(choice)
+            self.exit(event)
 
+    def on_number_key(self, event):
+        self._choose(int(event.char) - 1)
+        self.exit(event)
 
-@ requires_duration
-@ convert_masks_to_RGB
-def preview(clip, fps=15, audio=True, audio_fps=22050, audio_buffersize=3000,
-            audio_nbytes=2, fullscreen=False):
-    """
-    Displays the clip in a window, at the given frames per second
-    (of movie) rate. It will avoid that the clip be played faster
-    than normal, but it cannot avoid the clip to be played slower
-    than normal if the computations are complex. In this case, try
-    reducing the ``fps``.
+    def toggle_full_screen(self, _: tkinter.Event = None):
+        pass
+        """
+        self.is_full_screen = not self.is_full_screen
+        self.window.attributes("-fullscreen", self.is_full_screen)
+        self.video = (self.fullscreen_video if self.is_full_screen
+                      else self.native_video)
+        """
 
-    Parameters
-    ------------
+    def refresh_sources(self, _: tkinter.Event = None):
+        # TODO: refresh source clips
+        # self.video = self.assemble_video(self.callback())
+        pass
 
-    fps
-      Number of frames per seconds in the displayed video.
+    def display(self):
+        self.frame = self.video.get_frame(self.t)
+        self.photo = PIL.ImageTk.PhotoImage(
+            image=PIL.Image.fromarray(self.frame))
+        self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+        self.canvas.update()
 
-    audio
-      ``True`` (default) if you want the clip's audio be played during
-      the preview.
+    def update(self):
+        if not self.is_playing:
+            return
 
-    audio_fps
-      The frames per second to use when generating the audio sound.
+        # Get the current frame from the video source
+        start = time.time()
+        self.t = start - self.t0
+        if self.t >= self.video.duration:  # Repeat clips on loop
+            self.t0 = time.time()
+            self.t = 0
 
-    fullscreen
-      ``True`` if you want the preview to be displayed fullscreen.
+        self.display()
 
-    """
-    if fullscreen:
-        flags = pg.FULLSCREEN
-    else:
-        flags = 0
-
-    # compute and splash the first image
-    screen = pg.display.set_mode(clip.size, flags)
-
-    audio = audio and (clip.audio is not None)
-
-    if audio:
-        # the sound will be played in parrallel. We are not
-        # parralellizing it on different CPUs because it seems that
-        # pygame and openCV already use several cpus it seems.
-
-        # two synchro-flags to tell whether audio and video are ready
-        videoFlag = threading.Event()
-        audioFlag = threading.Event()
-        # launch the thread
-        audiothread = threading.Thread(target=clip.audio.preview,
-                                       args=(audio_fps,
-                                             audio_buffersize,
-                                             audio_nbytes,
-                                             audioFlag, videoFlag))
-        audiothread.start()
-
-    img = clip.get_frame(0)
-    imdisplay(img, screen)
-    if audio:  # synchronize with audio
-        videoFlag.set()  # say to the audio: video is ready
-        audioFlag.wait()  # wait for the audio to be ready
-
-    t0 = time.time()
-    for t in np.arange(1.0 / fps, clip.duration-.001, 1.0 / fps):
-
-        img = clip.get_frame(t)
-
-        for event in pg.event.get():
-            if event.type == pg.QUIT or \
-                    (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                if audio:
-                    videoFlag.clear()
-                return None
-            elif event.type == pg.MOUSEBUTTONDOWN:
-                pos = pg.mouse.get_pos()
-                if audio:
-                    videoFlag.clear()
-                return pos
-            elif event.type == pg.KEYDOWN and event.key in NUMBER_KEYS:
-                if audio:
-                    videoFlag.clear()
-                return abs(int(pg.key.name(event.key)))
-
-        t1 = time.time()
-        time.sleep(max(0, t - (t1-t0)))
-        imdisplay(img, screen)
-    return (-1, -1)
+        # Repeat update after delay, respecting framerate
+        stop = time.time()
+        lost_time = stop - start
+        wait = max(0, int(1000 * (self.dt - lost_time)))
+        self.update_id = self.window.after(wait, self.update)
